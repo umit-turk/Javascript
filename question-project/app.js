1b2a90df7cbac07fc0de44d09dc58355bf43f4f7
Ümit Yaşar TÜRK <76227146+umit-turk@users.noreply.github.com>
2021-04-17 00:24:49 +0300

    Add files via upload

---
+++ question-project/app.js
@@ -0,0 +1,24 @@
+//using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question){
+    const btn = question.querySelector(".question-btn");
+    btn.addEventListener('click', function(){
+        question.classList.toggle('show-text');
+    })
+
+})
+
+
+//traversing the dom
+
+/* const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const question = e.currentTarget.parentElement.parentElement;
+        console.log(question);
+        question.classList.toggle("show-text");
+    })
+}) */
